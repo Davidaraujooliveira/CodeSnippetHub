ce9b86ddcca2f7fdc0073d9fdadd9ee0cfad7f7d
Davidaraujooliveira <76564583+Davidaraujooliveira@users.noreply.github.com>
2024-08-08 15:20:51 +0300

    new update

--- db.py
+++ db.py
@@ -3,6 +3,19 @@ from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
 import sys
+from alembic import command
+from alembic.config import Config
+
+
+def setup_alembic_config(engine_url):
+    """
+    Set up and return the Alembic configuration.
+    """
+    alembic_cfg = Config()
+    alembic_cfg.set_main_option("script_location", "alembic")
+    alembic_cfg.set_main_option("sqlalchemy.url", engine_url)
+    return alembic_cfg
+
 
 try:
     load_dotenv()
@@ -25,3 +38,10 @@ try:
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 except Exception as e:
     sys.exit(f"Failed to create a SessionLocal instance: {e}")
+
+# Setup Alembic configuration and run migrations automatically
+alembic_cfg = setup_alembic_config(DATABASE_URL)
+try:
+    command.upgrade(alembic_cfg, "head")
+except Exception as e:
+    sys.exit(f"Failed to run Alembic migrations: {e}")
